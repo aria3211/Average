35794a576dc3bed63e9572dab423da7b05d49d4c
aria <aryashahbazi@gmail.com>
2021-03-23 00:09:04 +0430

    Done

---
+++ Build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: G:/C++/AW/New folder/Average
+# Build directory: G:/C++/AW/New folder/Average/Build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
